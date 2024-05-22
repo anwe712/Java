9fc29a222cf32f85cd80f097c15c55d4cebc09a5
anwe712 <ghoshanwesha272@gmail.com>
2024-05-22 17:33:01 +0530

    code added

---
+++ Patterns/Pattern4.java
@@ -0,0 +1,19 @@
+public class Pattern4 {
+    
+    public static void main(String[] args) {
+        int i , j , num=1 ;
+        for (i = 1 ; i <= 5 ; i++)
+        {
+            for (j = 1 ; j <= i ; j++)
+
+            {      
+                   System.out.print(num+" ");
+                   num ++ ;
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
