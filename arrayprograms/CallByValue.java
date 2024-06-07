ec674a3f83bd70730e959b50de850e908eb1726c
anwe712 <ghoshanwesha272@gmail.com>
2024-06-08 01:23:53 +0530

    added pdfs

--- arrayprograms/CallByValue.java
+++ arrayprograms/CallByValue.java
@@ -1,17 +1,17 @@
+//java always calls by value 
 package arrayprograms;
 
 public class CallByValue {
-    public static void modifyValue(int x) {
-        x = 10; // Modifying the value of the parameter x
+    public static void modifyValue(int num) {
+        num = 10; // Just assigning a new value to the parameter
+        System.out.println("After method call: " + num); 
     }
 
     public static void main(String[] args) {
         int num = 5;
-        System.out.println("Before method call: " + num);//5
-        modifyValue(num);//10
-        System.out.println("After method call: " + num);//5
+        System.out.println("Before method call: " + num); // 5
+        
+        modifyValue(num); // Call the method
+        System.out.println("After method call: " + num); // 5 (num is unchanged because Java is pass-by-value)
     }
 }
-
-
-    
